eef205692c3f0b2af21ad99fec96dc99e4f30173
prafulk <prafulprakash1993@gmail.com>
2014-12-24 18:07:34 +0400

    Implemented LG-LDA

---
+++ Octave/mlmaximisation.m
@@ -0,0 +1,19 @@
+#
+# Correlated Topic Model
+# Using CVB
+# The maximisation step (using ML estimates)
+#
+
+# Compute "better" model parameters
+function [M, S, B] = mlmaximisation( C, K, M, Si, B, Lambda, Nu, EN_jk, VN_jk )
+    [D,V] = size(C);
+
+    M = mean( Lambda, 1 );
+
+    for i = [1:D];
+        S = diag(Nu(i,:)) + (Lambda(i,:) - M)' * (Lambda(i,:) - M);
+    end;
+    S ./ D;
+
+    B = EN_jk + 1; # Actually proportional
+end;
