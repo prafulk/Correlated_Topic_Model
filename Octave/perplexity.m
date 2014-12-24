55e0764d624cd2353ca9012c99766e6b77f86de8
prafulk <prafulprakash1993@gmail.com>
2014-12-24 18:08:40 +0400

    Implementation using denominator bound

---
+++ Octave/perplexity.m
@@ -0,0 +1,23 @@
+#
+# Correlated Topic Model
+# Using CVB
+# Computing perplexity
+#
+
+# Compute the per-word perplexity of data
+function P = perplexity( C, B, K, phi )
+    N = sum( sum( C ) );
+    [D,V] = size(C);
+
+    # Convert to p-dist
+    logB = safe_log(B);
+    logB = B - repmat( logsum( B, 2 ), 1, V );
+
+    P = 0;
+    perp = zeros( D*V, K );
+    for j = [1:K];
+        perp(:, j) = (safe_log(phi{j}) + repmat( logB(j,:), D, 1 ) )(:);
+    end;
+    P = sum( logsum( perp ) );
+    P = exp( - P ./ N );
+end;
